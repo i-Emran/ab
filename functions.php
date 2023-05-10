ddff176f6dd7fd3a44b2a09d57af5a14c99d8837
Emran <44391976+i-Emran@users.noreply.github.com>
2023-05-10 12:17:30 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+
+require_once get_template_directory().'/lib/updater.php';
+
+add_action('after_setup_theme', function() {
+
+	//get_template_part('lib/Updater');
+});
+
+
+
+new Updater(
+	[
+		'name' => 'ab', // Theme Name.
+		'repo' => 'i-Emran/ab', // Theme repository.
+		'slug' => 'ab', // Theme Slug.
+		'url' => 'https://github.com/i-Emran/ab', // Theme URL.
+		'ver' => 1.2 // Theme Version.
+	]
+);
